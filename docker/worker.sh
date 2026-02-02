01364311bb7ab03be2887686f0e2efaf24089a96
Jorge Junior <jorgejuniordev@gmail.com>
2026-02-02 20:14:16 -0300

    Disable access logs in Docker entrypoints (#17)
    * Disable access logs in Docker entrypoints
    
    * Remove --no-access-log from Octane

--- docker/worker.sh
+++ docker/worker.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Start health check server in background on port 2019 (matches Easypanel health check)
-HEALTH_SERVICE=worker php -S 0.0.0.0:2019 /app/docker/health.php &
+HEALTH_SERVICE=worker php -q -S 0.0.0.0:2019 /app/docker/health.php &
 
 # Start the queue worker (exec makes this PID 1)
 exec php artisan queue:work --sleep=3 --tries=3 --max-time=3600 --verbose
