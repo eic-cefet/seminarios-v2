35e078de43e5f30d160d06a4cd6776a8d30fd305
Jorge Junior <jorgejuniordev@gmail.com>
2026-04-23 19:24:13 -0300

    feat(mail): add NewSeminarAlert email

---
+++ app/Mail/NewSeminarAlert.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Seminar;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Headers;
+use Illuminate\Queue\SerializesModels;
+
+class NewSeminarAlert extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public User $user,
+        public Seminar $seminar,
+    ) {}
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Novo seminário: '.$this->seminar->name.' - '.config('mail.name'),
+        );
+    }
+
+    public function headers(): Headers
+    {
+        return new Headers(
+            text: [
+                'X-Entity-Ref-ID' => $this->refId(),
+                'X-Mail-Class' => self::class,
+            ],
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.new-seminar-alert',
+            with: [
+                'userName' => $this->user->name,
+                'seminar' => $this->seminar,
+            ],
+        );
+    }
+
+    protected function refId(): string
+    {
+        return "seminar-alert:{$this->seminar->id}:{$this->user->id}";
+    }
+}
---
+++ resources/views/emails/new-seminar-alert.blade.php
@@ -0,0 +1,36 @@
+<x-mail::message>
+# Novo Seminário Disponível
+
+Olá, **{{ $userName }}**!
+
+Um novo seminário combina com suas preferências de alerta.
+
+<x-mail::panel>
+**{{ $seminar->name }}**
+@if ($seminar->scheduled_at)
+
+**Quando:** {{ $seminar->scheduled_at->format('d/m/Y') }} às {{ $seminar->scheduled_at->format('H:i') }} (horário de Brasília)
+@endif
+@if ($seminar->seminarLocation)
+
+**Local:** {{ $seminar->seminarLocation->name }}
+@endif
+</x-mail::panel>
+
+@if ($seminar->description)
+{{ \Illuminate\Support\Str::limit(strip_tags($seminar->description), 400) }}
+@endif
+
+<x-mail::button :url="url('/seminario/' . $seminar->slug)">
+Ver Detalhes do Seminário
+</x-mail::button>
+
+Você está recebendo este e-mail porque ativou os alertas de novos seminários. Para alterar ou desativar, acesse suas preferências.
+
+Atenciosamente,<br>
+{{ config('mail.team_name') }}
+
+<x-mail::subcopy>
+Este é um e-mail automático. Por favor, não responda.
+</x-mail::subcopy>
+</x-mail::message>
