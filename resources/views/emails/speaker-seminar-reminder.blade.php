20b82edec88e404774524a2bd32a2b2b562c6ec0
Jorge Junior <jorgejuniordev@gmail.com>
2026-04-26 15:35:28 -0300

    feat(comm): add SpeakerSeminarReminder mailable

---
+++ resources/views/emails/speaker-seminar-reminder.blade.php
@@ -0,0 +1,29 @@
+<x-mail::message>
+# Lembrete: você está apresentando amanhã
+
+Olá, **{{ $speakerName }}**!
+
+Lembre-se: amanhã você está apresentando o seminário abaixo.
+
+<x-mail::panel>
+**{{ $seminar->name }}**
+
+@if($seminar->scheduled_at)
+**Data:** {{ $seminar->scheduled_at->format('d/m/Y') }}<br>
+**Horário:** {{ $seminar->scheduled_at->format('H:i') }} (horário de Brasília)
+@endif
+@if($seminar->seminarLocation)
+<br>**Local:** {{ $seminar->seminarLocation->name }}
+@endif
+@if($seminar->room_link)
+<br>**Link da sala:** {{ $seminar->room_link }}
+@endif
+</x-mail::panel>
+
+<x-mail::button :url="url('/seminario/' . $seminar->slug)">
+Ver detalhes
+</x-mail::button>
+
+Boa apresentação!<br>
+{{ config('mail.name') }}
+</x-mail::message>
---
+++ tests/Feature/Mail/SpeakerSeminarReminderTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Mail\SpeakerSeminarReminder;
+use App\Models\Seminar;
+use App\Models\User;
+
+it('renders speaker-targeted copy', function () {
+    $speaker = User::factory()->create(['name' => 'Prof. Lima']);
+    $seminar = Seminar::factory()->create(['name' => 'Algoritmos', 'scheduled_at' => now()->addDay()]);
+
+    $mailable = new SpeakerSeminarReminder($speaker, $seminar);
+
+    $mailable->assertSeeInHtml('Prof. Lima');
+    $mailable->assertSeeInHtml('Algoritmos');
+    $mailable->assertSeeInHtml('você está apresentando');
+    $mailable->assertHasSubject('Você apresenta amanhã: Algoritmos - '.config('mail.name'));
+});
